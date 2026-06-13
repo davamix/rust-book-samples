e4d8f68e99a8572e7d6a3be8f244f166243372e2
Daniel Valcarce <davamix@gmail.com>
2026-06-13 21:36:51 +0000

    Full implementation of Guess a number sample

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,15 +1,38 @@
+// https://rust-book.cs.brown.edu/ch02-00-guessing-game-tutorial.html
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number");
 
-    println!("Write your number:");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    // println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Cannot read the number");
+    loop {
+        println!("Write your number:");
 
-    println!("Your number is {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Cannot read the number");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your number is {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
