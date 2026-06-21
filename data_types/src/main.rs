bb8f1b1aade180bc8f14c0a13c36411498f2ba53
Daniel Valcarce <davamix@gmail.com>
2026-06-21 05:29:05 +0000

    Fix character. Tuples

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,9 +1,17 @@
 #![allow(unused)]
 fn main() {
-    let guess = "42".parse().expect("Not a number");
+    let guess: u32 = "42".parse().expect("Not a number");
 
     // characters
     let c = 'z';
-    let z: char = 'ℤ'
+    let z: char = 'ℤ';
     let heart_eyed_cat = '😻';
+
+    // tuples
+    let tup:(i32, f64, u8) = (500, 6.4, 1);
+
+    // tuples + pattern matching
+    let tup2 = (500, 6.4, 1);
+    let (x, y, z) = tup2;
+    println!("The value of y is: {y}")
 }
