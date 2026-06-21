97f2db29b156eefe1cddd53643330bff30490b2d
Daniel Valcarce <davamix@gmail.com>
2026-06-21 05:41:53 +0000

    Tuple accessibility

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -13,5 +13,12 @@ fn main() {
     // tuples + pattern matching
     let tup2 = (500, 6.4, 1);
     let (x, y, z) = tup2;
-    println!("The value of y is: {y}")
+    println!("The value of y is: {y}");
+
+    let first_value = tup2.0;
+    let second_value = tup2.1;
+    
+    println!("first_value: {first_value}");
+    // println!("third_value: {tup2.2}"); // Not supported
+    println!("third_value: {0}", tup2.2);
 }
